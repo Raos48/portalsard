890b34803d9d16000d12b28ed735a6f935dc8d87
Raos48 <131822770+Raos48@users.noreply.github.com>
2024-04-09 02:33:35 -0300

    ALteração de código hostinger 02:31

--- teste.py
+++ teste.py
@@ -1,29 +1,13 @@
-import pandas as pd
-from app import SQLALCHEMY_DATABASE_URI, Estoque
-import numpy as np
-# Caminho do seu arquivo Excel
-excel_path = 'Pasta1.xlsx'
+from dotenv import load_dotenv
+import os
 
-# Substitui valores NaN por None (que será interpretado como NULL pelo MySQL)
-df_excel = pd.read_excel(excel_path)
-df_excel = df_excel.where(pd.notnull(df_excel), None)
+load_dotenv()  # Carrega as variáveis de ambiente do arquivo .env
 
+# Imprime as variáveis de ambiente para debugar
+print("DEBUG:", os.getenv("DEBUG"))
+print("USER:", os.getenv("USER"))
+print("PASSWORD:", os.getenv("PASSWORD"))
+print("SERVER:", os.getenv("SERVER"))
+print("DB:", os.getenv("DB"))
 
-
-
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
-
-# Criar a engine e a sessão para o SQLAlchemy
-engine = create_engine(SQLALCHEMY_DATABASE_URI)
-Session = sessionmaker(bind=engine)
-session = Session()
-
-for index, row in df_excel.iterrows():
-    estoque_item = session.query(Estoque).filter(Estoque.id == row['id']).first()
-    if estoque_item:
-        # Se 'situacao' é NaN, substitua por None ou outro valor adequado
-        situacao_value = row['situacao'] if not pd.isna(row['situacao']) else None
-        estoque_item.situacao = situacao_value
-        session.commit()
 
